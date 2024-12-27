7ef39822c2fb003ea8652a26c2f87b277ac6754c
MD IRFAN RAJ <mdirfanraj88.omega@gmail.com>
2024-12-27 10:58:12 +0530

    Initial commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer fixed w-full left-0 bottom-0 flex justify-center items-center">
+          Copyright&copy; 2024
+          <p>Build by Md Irfan Raj</p>
+    </footer>
+  );
+}
+
+export default Footer;
