706feb29739d82e7a8b15c846833a133b65076e5
MD IRFAN RAJ <mdirfanraj88.omega@gmail.com>
2025-03-19 16:00:14 +0530

    Update authService.js added backend url

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://ey-news-app-server.onrender.com/api/auth';
 
 export const signup = (data) => axios.post(`${API_URL}/signup`, data);
 export const login = (data) => axios.post(`${API_URL}/login`, data);
